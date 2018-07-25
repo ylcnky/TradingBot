175e85bf7f62641e4b3996f74a832e00417aa5b0
ylcnky <mehmet.yalcinkaya@aalto.fi>
2018-07-25 23:36:04 +0300

    make keys.py file for other exchange platform

---
+++ Keys.py
@@ -0,0 +1,7 @@
+"""
+The Keys.py Program is for keeping track of Binance Keys for use with ylcnkyBot.py and
+save_historical_data_ylcnky.py
+"""
+
+Key1 = { 'api_key': 'SccVh1gfjzNz1Kgjg6g1YoIwFsGfNvriEVCVGcPGzdaW8hiMx6MELvK9Sujephq6',
+          'api_secret':'l5ipEseTolbviJXwVBkr3P3zx1mcjMpLPmWDfZN2oVxXBF73frcHor3nRYuBsiFA'}
